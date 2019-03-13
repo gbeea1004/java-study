413eb6c695757a8a400a532202dceed2ad15d544
geonHee <corqkrtk3@naver.com>
2019-03-13 23:59:00 +0900

    배열 복사 : System.arraycopy() 활용

---
+++ src/test/java/array/ArrayTest.java
@@ -0,0 +1,23 @@
+package array;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayTest {
+    @Test
+    public void arrayCopy() {
+        String[] students = {"피터", "라임", "스컬", "레드", "레시안"};
+        String[] newStudents = new String[students.length];
+        System.arraycopy(students, 1, newStudents, 0, 1);
+        assertThat(newStudents[0]).isEqualTo("라임");
+        assertThat(newStudents[1]).isEqualTo(null);
+
+        System.arraycopy(students, 0, newStudents, 0, students.length);
+        assertThat(newStudents[0]).isEqualTo("피터");
+        assertThat(newStudents[1]).isEqualTo("라임");
+        assertThat(newStudents[2]).isEqualTo("스컬");
+        assertThat(newStudents[3]).isEqualTo("레드");
+        assertThat(newStudents[4]).isEqualTo("레시안");
+    }
+}
