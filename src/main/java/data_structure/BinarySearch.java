472c892b336e63ded2f9eac133d562c101325170
geonHee <corqkrtk3@naver.com>
2019-03-18 16:48:54 +0900

    자료구조 : 재귀를  이용한 이진탐색

--- src/main/java/data_structure/BinarySearch.java
+++ src/main/java/data_structure/BinarySearch.java
@@ -2,21 +2,36 @@ package data_structure;
 
 class BinarySearch {
 
-    int start(int[] arr, int searchNo) {
+    int start(int[] numbers, int searchNo) {
         int first = 0;
-        int last = arr.length - 1;
+        int last = numbers.length - 1;
         int mid;
 
         while (first <= last) {
             mid = (first + last) / 2;
-            if (arr[mid] == searchNo) {
+            if (numbers[mid] == searchNo) {
                 return mid;
-            } else if (arr[mid] > searchNo) {
+            } else if (numbers[mid] > searchNo) {
                 last = mid - 1;
             } else {
                 first = mid + 1;
             }
         }
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] numbers, int first, int last, int target) {
+        int mid;
+        if (first > last) {
+            return -1;
+        }
+        mid = (first + last) / 2;
+        if (numbers[mid] == target) {
+            return mid;
+        } else if (numbers[mid] > target) {
+            return recursiveBinarySearch(numbers, first, mid - 1, target);
+        } else {
+            return recursiveBinarySearch(numbers, mid + 1, last, target);
+        }
     }
 }
--- src/test/java/data_structure/BinarySearchTest.java
+++ src/test/java/data_structure/BinarySearchTest.java
@@ -16,7 +16,16 @@ public class BinarySearchTest {
         assertThat(binarySearch.start(numbers, 7)).isEqualTo(3);
         assertThat(binarySearch.start(numbers, 9)).isEqualTo(4);
         assertThat(binarySearch.start(numbers, 10)).isEqualTo(-1);
+    }
 
-
+    @Test
+    public void 재귀를_이용한_이진탐색() {
+        int[] numbers = {1, 3, 5, 7, 9};
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 1)).isEqualTo(0);
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 3)).isEqualTo(1);
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 5)).isEqualTo(2);
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 7)).isEqualTo(3);
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 9)).isEqualTo(4);
+        assertThat(BinarySearch.recursiveBinarySearch(numbers, 0, numbers.length - 1, 10)).isEqualTo(-1);
     }
 }
