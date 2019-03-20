71230dbfeaa8125f8ee157ddda151ff450dd54bf
geonHee <corqkrtk3@naver.com>
2019-03-20 22:56:04 +0900

    AssertJ containsExactly() Test

---
+++ src/test/java/ContainsExactlyTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContainsExactlyTest {
+    @Test
+    public void 모든값이_포함되어있는가() {
+        List<String> alphabet = new ArrayList<>();
+        alphabet.add("A");
+        alphabet.add("B");
+        alphabet.add("C");
+        assertThat(alphabet).containsExactly("A", "B", "C");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void 모든값이_포함되어있지_않다면_에러() {
+        String[] s = {"1", "2"};
+        assertThat(s).containsExactly("1");
+    }
+}
