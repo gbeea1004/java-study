871e633014580c14209da43d4bf2da539f575383
geonHee <corqkrtk3@naver.com>
2019-04-28 23:38:18 +0900

    Lamda, Functional Interface 공부

---
+++ src/test/java/LamdaTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LamdaTest {
+    @Test
+    public void 람다식안쓸때() {
+        assertThat(max(1, 2)).isEqualTo(2);
+    }
+
+    private int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    @Test
+    public void 람다식쓸때() {
+        Calculator calculator = (int a, int b) -> a > b ? a : b;
+        assertThat(calculator.max(1, 2)).isEqualTo(2);
+    }
+}
+
+@FunctionalInterface // 함수형 인터페이스
+interface Calculator {
+    public abstract int max(int a, int b);
+}
