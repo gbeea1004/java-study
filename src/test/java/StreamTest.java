c192df1feb6b5db18f526b683db9274df68d3190
geonHee <corqkrtk3@naver.com>
2019-06-03 17:01:21 +0900

    Stream map, flatMap 학습

--- src/test/java/StreamTest.java
+++ src/test/java/StreamTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import stream.User;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +11,27 @@ public class StreamTest {
     public void 연산() {
         Stream<String> stream = Stream.of("1", "2");
         assertThat(stream.count()).isEqualTo(2);
+    }
+
+    @Test
+    public void map() {
+        Stream<User> userStream = Stream.of(
+                new User("건희", 27),
+                new User("도비", 1),
+                new User("우디", 24)
+        );
+        userStream.map(User::getName).forEach(System.out::println);
+    }
+
+    @Test
+    public void flatMap() {
+        Stream<String[]> stringArrStream = Stream.of(
+                new String[]{"abc", "라임", "임다"},
+                new String[]{"비와이", "제이플라", "제시"},
+                new String[]{"호눅스", "JK", "포비"}
+        );
+
+        System.out.println("flatMap 사용 시");
+        stringArrStream.flatMap(Arrays::stream).forEach(System.out::println);
     }
 }
