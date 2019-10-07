8a95314adde9fa1c3de375b0282f6a793a320775
geonHee <corqkrtk3@naver.com>
2019-10-07 18:20:25 +0900

    Object chep 2 : [Add] Movie class

--- src/main/java/books/object/movie_reservation/Movie.java
+++ src/main/java/books/object/movie_reservation/Movie.java
@@ -1,4 +1,15 @@
 package books.object.movie_reservation;
 
 public class Movie {
+    private String title;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+
+    public Money getFee() {
+        return fee;
+    }
 }
