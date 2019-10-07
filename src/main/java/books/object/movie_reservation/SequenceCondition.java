0f02ea7778c67de19f3f2c32d8e61cb7538b0f94
geonHee <corqkrtk3@naver.com>
2019-10-07 20:15:46 +0900

    Object chep 2 : [Add] SequenceCondition class

---
+++ src/main/java/books/object/movie_reservation/SequenceCondition.java
@@ -0,0 +1,14 @@
+package books.object.movie_reservation;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.isSequence(sequence);
+    }
+}
