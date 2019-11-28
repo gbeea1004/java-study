8fa1868233c179d3b8ca6b50ab9dc9a6d605e6b9
geonHee <corqkrtk3@naver.com>
2019-11-28 13:26:01 +0900

    숫자 야구 게임 TDD로 구현하기 - 테스트 케이스 리팩토링

--- src/test/java/baseball_game/BaseballGameTest.java
+++ src/test/java/baseball_game/BaseballGameTest.java
@@ -7,13 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BaseballGameTest {
     @Test
     public void nomatch() {
-        // 정답 479
-        Game game = new Game(479);
+        Game game = new Game(479); // 정답 479
 
-        // 예측 123
-        Score score = game.guess(123);
+        Score score = game.guess(123); // 예측 123
+        assertNoMatch(score); // 0 스트라이크 0 볼
+        
+        Score score2 = game.guess(568); // 예측 568
+        assertNoMatch(score2); // 0 스트라이크 0 볼
+    }
 
-        // 0 스트라이크 0 볼
+    public void assertNoMatch(Score score) {
         assertThat(score.strikes()).isEqualTo(0);
         assertThat(score.balls()).isEqualTo(0);
     }
