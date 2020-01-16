29030031a1d6914e2ba6572b1342856744e2ebf7
gunny <corqkrtk3@naver.com>
2020-01-16 23:53:16 +0900

    gradle-wrapper.properties 버전 변경

--- src/main/java/java8_interface/Main.java
+++ src/main/java/java8_interface/Main.java
@@ -2,9 +2,9 @@ package java8_interface;
 
 public class Main {
     public static void main(String[] args) {
-        Control control = new Avante();
-        System.out.println(control.open());
-        System.out.println(control.start());
+//        Control control = new Avante();
+//        System.out.println(control.open());
+//        System.out.println(control.start());
 
 
     }
