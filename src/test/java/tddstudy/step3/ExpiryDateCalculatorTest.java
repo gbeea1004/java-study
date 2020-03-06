b2265be9a2a4fe9dce5062b479d269607cf46c89
geonHee <corqkrtk3@naver.com>
2020-03-06 16:30:15 +0900

    Test TDD study ExpiryDateCalculatorTest method deduplication

--- src/test/java/tddstudy/step3/ExpiryDateCalculatorTest.java
+++ src/test/java/tddstudy/step3/ExpiryDateCalculatorTest.java
@@ -10,20 +10,21 @@ public class ExpiryDateCalculatorTest {
 
     @Test
     public void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2020, 3, 6);
-        int payAmount = 10000;
+        assertExpiryDate(
+                LocalDate.of(2020, 3, 6),
+                10000,
+                LocalDate.of(2020, 4, 6));
 
+        assertExpiryDate(
+                LocalDate.of(2020, 1, 1),
+                10000,
+                LocalDate.of(2020, 2, 1)
+        );
+    }
+
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator calculator = new ExpiryDateCalculator();
         LocalDate expiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
-
-        assertThat(expiryDate).isEqualTo(LocalDate.of(2020, 4, 6));
-
-        LocalDate billingDate2 = LocalDate.of(2020, 1, 1);
-        int payAmount2 = 10000;
-
-        ExpiryDateCalculator calculator2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = calculator2.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertThat(expiryDate2).isEqualTo(LocalDate.of(2020, 2, 1));
+        assertThat(expiryDate).isEqualTo(expectedExpiryDate);
     }
 }
