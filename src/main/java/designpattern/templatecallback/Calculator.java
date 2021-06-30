40e9fa8a6180ebfa9f8d37a8b10014f4678d775c
gbeea1004 <jvckigh@gmail.com>
2021-06-30 21:07:53 +0900

    템플릿 콜백 패턴 : Try-with-resoures, 람다 적용하여 리팩토링

--- src/main/java/designpattern/templatecallback/Calculator.java
+++ src/main/java/designpattern/templatecallback/Calculator.java
@@ -1,62 +1,43 @@
 package designpattern.templatecallback;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
 
     public int calcSum(String filePath) {
-        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-            @Override
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer result = 0;
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    result += Integer.valueOf(line);
-                }
-                return result;
+        BufferedReaderCallback sumCallback = br -> {
+            Integer result = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                result += Integer.parseInt(line);
             }
+            return result;
         };
 
         return fileReadTemplate(filePath, sumCallback);
     }
 
     public int calcMultiply(String filePath) {
-        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
-            @Override
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer result = 1;
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    result *= Integer.valueOf(line);
-                }
-                return result;
+        BufferedReaderCallback multiplyCallback = br -> {
+            Integer result = 1;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                result *= Integer.parseInt(line);
             }
+            return result;
         };
 
         return fileReadTemplate(filePath, multiplyCallback);
     }
 
     private Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filePath));
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             return callback.doSomethingWithReader(br);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        return 0;
+        throw new IllegalArgumentException("Calculator Error");
     }
 }
